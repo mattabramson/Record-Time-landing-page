0b47194e81c206ff4ab1607724c957e2b47bbcec
mattabramson <60493857+mattabramson@users.noreply.github.com>
2020-04-27 20:40:21 -0400

    Made sound buttons reset and play when clicked

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ const woo = document.querySelector('#wooIcon');
 const drum = document.querySelector('#drumIcon');
 const stop = document.querySelector('#drumStop');
 
+
+
 drum.onclick = () => {
   document.getElementById(('drum')).play();
 };
@@ -16,21 +18,27 @@ stop.onclick = () => {
   document.getElementById(('drum')).pause();
 };
 guitarOne.onclick = () => {
+  document.getElementById(('guitarOne')).currentTime = 0;
   document.getElementById(('guitarOne')).play();
 };
 guitarTwo.onclick = () => {
+  document.getElementById(('guitarTwo')).currentTime = 0;
   document.getElementById(('guitarTwo')).play();
 };
 guitarThree.onclick = () => {
+  document.getElementById(('guitarThree')).currentTime = 0;
   document.getElementById(('guitarThree')).play();
 }
 synthOne.onclick = () => {
+  document.getElementById(('synthOne')).currentTime = 0;
   document.getElementById(('synthOne')).play();
 };
 synthTwo.onclick = () => {
+  document.getElementById(('synthTwo')).currentTime = 0;
   document.getElementById(('synthTwo')).play();
 };
 woo.onclick = () => {
+  document.getElementById(('woo')).currentTime = 0;
   document.getElementById(('woo')).play();
 };
 //April 2020
